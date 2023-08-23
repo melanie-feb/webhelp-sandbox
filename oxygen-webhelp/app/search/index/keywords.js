ad24329e2560da3253659ec62ef4b1d527b4ee34
Attaullah Syed <attaullah.s@hcl.com>
2023-08-23 19:42:48 +0530

    test

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,33 @@
+var keywords=[{w:"Overview",p:["p0"]},{w:"of",p:["p0"]},{w:"HCL",p:["p0"]},{w:"ZAO",p:["p0"]},{w:"V2.1.1",p:["p0"]},{w:"RPHE0115W",p:["p1"]},{w:"running",p:["p2","p4","p5"]},{w:"Analyzer",p:["p2","p3","p5"]},{w:"in",p:["p2","p4","p5","p15"]},{w:"batch",p:["p2","p4","p5"]},{w:"mode",p:["p2","p4","p5"]},{w:"the",p:["p5","p15","p24","p26"]},{w:"HSIAHZAA001E",p:["p6"]},{w:"Hello",p:["p7"]},{w:"Oil",p:["p8","p15"]},{w:"Paint",p:["p9"]},{w:"Water",p:["p10"]},{w:"hose",p:["p10"]},{w:"Windshield",p:["p11"]},{w:"washer",p:["p11"]},{w:"fluid",p:["p11"]},{w:"Context",p:["p12"]},{w:"Sensitive",p:["p12"]},{w:"Help",p:["p12"]},{w:"Snow",p:["p13","p14"]},{w:"shovel",p:["p13"]},{w:"Shovelling",p:["p14"]},{w:"Changing",p:["p15"]},{w:"your",p:["p15"]},{w:"car",p:["p15","p26"]},{w:"while",p:["p15"]},{w:"you",p:["p15"]},{w:"are",p:["p15"]},{w:"vacationing",p:["p15"]},{w:"syntax",p:["p16","p23","p24"]},{w:"diagrams",p:["p16","p23","p24"]},{w:"how",p:["p17","p24"]},{w:"to",p:["p17","p24"]},{w:"read",p:["p17","p24"]},{w:"variables",p:["p18"]},{w:"keywords",p:["p19"]},{w:"repeatable",p:["p20"]},{w:"items",p:["p20"]},{w:"fragments",p:["p21"]},{w:"commands",p:["p22"]},{w:"reading",p:["p23"]},{w:"Configuring",p:["p25"]},{w:"an",p:["p25"]},{w:"Source",p:["p25"]},{w:"Oracle",p:["p25"]},{w:"database",p:["p25"]},{w:"with",p:["p25"]},{w:"Enterprise",p:["p25"]},{w:"Server",p:["p25"]},{w:"Washing",p:["p26"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3, 4];
+ph["p1"]=[5];
+ph["p2"]=[6, 7, 8, 9, 10];
+ph["p3"]=[7];
+ph["p4"]=[6, 8, 9, 10];
+ph["p5"]=[6, 11, 7, 8, 9, 10];
+ph["p6"]=[12];
+ph["p7"]=[13];
+ph["p8"]=[14];
+ph["p9"]=[15];
+ph["p10"]=[16, 17];
+ph["p12"]=[21, 22, 23];
+ph["p11"]=[18, 19, 20];
+ph["p14"]=[26, 24];
+ph["p13"]=[24, 25];
+ph["p16"]=[34, 35];
+ph["p15"]=[27, 11, 14, 8, 28, 29, 30, 31, 32, 33];
+ph["p18"]=[39];
+ph["p17"]=[36, 37, 38];
+ph["p19"]=[40];
+ph["p21"]=[43];
+ph["p20"]=[41, 42];
+ph["p23"]=[45, 34, 35];
+ph["p22"]=[44];
+ph["p25"]=[46, 47, 48, 49, 50, 51, 52, 53];
+ph["p24"]=[36, 37, 38, 11, 34, 35];
+ph["p26"]=[54, 11, 29];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {0:4,12:4,3:-1,14:4,1:4,4:-1,13:4,15:4,2:4};
